d2ed79f70849c8ec44b8ac75d2e26c3c48a6556f
Jasna <ansaj@hotmail.com>
2020-12-25 15:12:15 +0100

    Add script to menu modal

--- dist/all.js
+++ dist/all.js
@@ -1,1 +1,1 @@
-$(".header__icon").on("click",function(){$(this).toggleClass("active"),$("nav").toggleClass("open")}),$("nav a").on("click",function(){$(".header__icon").toggleClass("active"),$("nav").removeClass("open")});
+$(".header__icon").on("click",function(){$(this).toggleClass("active"),$("nav").toggleClass("open")}),$("nav a").on("click",function(){$(".header__icon").toggleClass("active"),$("nav").removeClass("open")}),$(".menu__button").on("click",function(){$(".modal").show()}),$(".modal__icon").on("click",function(){$(".modal").hide()});
--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,13 @@ $('nav a').on('click',function(){
     $('.header__icon').toggleClass('active');
     $('nav').removeClass('open');
 })
+
+/*show modal for menu*/
+
+$('.menu__button').on('click',function() {
+    $('.modal').show();
+})
+
+$('.modal__icon').on('click',function() {
+    $('.modal').hide();
+})
