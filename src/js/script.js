bf34fb0ceb8721825301e79d9eb6302511389493
Jasna <ansaj@hotmail.com>
2020-12-29 18:22:30 +0100

    Applying slick carousel on dishes section

--- src/js/script.js
+++ src/js/script.js
@@ -127,24 +127,43 @@ function showImages(n) {
     images[imageIndex - 1].style.display = "flex";
 }
 
-//carousel
+/*carousel*/
 
-var cardIndex = 1;
-showCard(cardIndex);
-
-function currentCard(n) {
-    showCard(cardIndex = n);
-}
-
-function showCard() {
-    var i;
-    var cards = document.getElementsByClassName('dish__wrap');
-    var dots = document.getElementsByClassName('dishes__dot');
-
-
-    for(i = 0; i < dots.length; i++) {
-        dots[i].style.background = "#b7b7b7";
-    }
-    
-    dots[cardIndex - 1].style.background = "#f34949";
-}
+ $('.dishes__wrap').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: false,
+            dots: true
+        }
+        },
+        {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots:true
+        }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+              
